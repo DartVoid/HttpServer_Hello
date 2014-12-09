2ce9d591fcacc5560e050b22b3b097c0d1a1d644
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2014-12-09 11:51:44 +0100

    First commit

---
+++ bin/server.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  var portEnv = Platform.environment['PORT'];
+  var port = portEnv == null ? 9090 : int.parse(portEnv);
+
+  HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, port).then((HttpServer server) {
+    print('Listening on localhost, port ${server.port}');
+    server.listen((HttpRequest request) {
+      request.response.write('Hello, world!');
+      request.response.close();
+    });
+  }).catchError((e) => print(e.toString()));
+}
+
