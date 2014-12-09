72e4a3ab41363445284c3e3b29f966257310bfad
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2014-12-09 11:56:30 +0100

    Added output on server

--- bin/server.dart
+++ bin/server.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+
 void main() {
   var portEnv = Platform.environment['PORT'];
   var port = portEnv == null ? 9090 : int.parse(portEnv);
@@ -7,7 +8,8 @@ void main() {
   HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, port).then((HttpServer server) {
     print('Listening on localhost, port ${server.port}');
     server.listen((HttpRequest request) {
-      request.response.write('Hello, world!');
+      print("${new DateTime.now()}: Hello server output!");
+      request.response.write('Hello world!');
       request.response.close();
     });
   }).catchError((e) => print(e.toString()));
