c33d0764ebab73b432756c75365964bd630ad5b4
Andreas Ölund <andreas@dartvoid.com>
2015-02-25 17:31:53 +0100

    Added env variable ADDRESS

--- bin/server.dart
+++ bin/server.dart
@@ -4,8 +4,11 @@ void main() {
   var portEnv = Platform.environment['PORT'];
   var port = portEnv == null ? 9090 : int.parse(portEnv);
 
-  HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, port).then((HttpServer server) {
-    print('Listening on localhost, port ${server.port}');
+  var addressEnv = Platform.environment['ADDRESS'];
+  var address = addressEnv == null ? InternetAddress.LOOPBACK_IP_V4 : addressEnv;
+
+  HttpServer.bind(address, port).then((HttpServer server) {
+    print('Listening on address: ${server.address}, port: ${server.port}');
     server.listen((HttpRequest request) {
       print("${new DateTime.now()}: Hello server output!");
       request.response.write('Hello world!');
